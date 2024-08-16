b3d58e8a5dbbc4130eba9e7af727d9df4e82a534
zeynepesrai <zeynepesraisler@gmail.com>
2024-08-16 06:36:32 -0700

    changes

---
+++ crm_analytics.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import datetime as dt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+pd.options.display.float_format = '{:,.2f}'.format
+
+df_ = pd.read_excel(
+    "C:/Users/zei/Desktop/CRM_analytics/Kurs Materyalleri(CRM_Analytics)/datasets/online_retail_II.xlsx",
+    sheet_name="Year 2010-2011")
+df = df_.copy()
+
+df.head()
+df.isnull().sum()
+
+df["Description"].nunique()
+df["Description"].value_counts().head()
+df.groupby("Description").agg({"Quantity": "sum"}).head()
+df.groupby("Description").agg({"Quantity": "sum"}).sort_values("Quantity", ascending=False).head()
+
+df["TotalPrice"] = df["Quantity"] * df["Price"]
+df.groupby("Invoice").agg({"TotalPrice": "sum"}).head()
+
+#Data Preparation
+
+df.shape
+df.dropna(inplace=True)
+df.shape
+df.describe().T
+
+df[~df["Invoice"].str.contains("C", na=False)]  #iade ürünlerin veri setinden çıkarılması
+
+#RFM Metrics
+
+df["InvoiceDate"].max()
+today_date = dt.datetime(2011, 12, 11)  # Analiz tarihi, son tarihe iki gül eklenerek oluşturuldu.
+
+rfm = df.groupby("Customer ID").agg({'InvoiceDate': lambda InvoiceDate: (today_date - InvoiceDate.max()).days,
+                                     'Invoice': lambda Invoice: Invoice.nunique(),
+                                     'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+rfm.head()
+
+rfm.columns = ['recency', 'frequency', 'monetary']
+rfm.describe().T
+
+rfm = rfm[rfm["monetary"] > 0]
+rfm.describe().T
+
+#RFM Scores
+# Recency: < 5 Frequency: > 5 Monetary: > 5
+
+rfm["recency_score"] = pd.qcut(rfm["recency"], 5, labels=[5, 4, 3, 2, 1])
+rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+rfm["monetary_score"] = pd.qcut(rfm["monetary"], 5, labels=[1, 2, 3, 4, 5])
+
+#RF Score
+rfm["RFM_SCORE"] = (rfm["recency_score"].astype(str) +
+                    rfm["frequency_score"].astype(str))
+
+seg_map = {
+    r'[1-2][1-2]': 'hibernating',
+    r'[1-2][3-4]': 'at_Risk',
+    r'[1-2]5': 'cant_lose',
+    r'3[1-2]': 'about_to_sleep',
+    r'33': 'need_attention',
+    r'[3-4][4-5]': 'loyal_customers',
+    r'41': 'promising',
+    r'51': 'new_customers',
+    r'[4-5][2-3]': 'potential_loyalists',
+    r'5[4-5]': 'champions',
+}
+
+rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
+
+rfm[["segment", "recency", "frequency", "monetary"]].groupby("segment").agg({"mean", "count"})
+
+rfm[rfm["segment"] == 'need_attention'].head()
+rfm[rfm["segment"] == 'cant_lose'].index
+
+new_df = pd.DataFrame()
+new_df["new_customer_id"] = rfm[rfm["segment"] == "new_customers"].index
+new_df["new_customer_id"] = new_df["new_customer_id"].astype(int)
+new_df.to_csv("new_customers.csv")
+rfm.to_csv("rfm.csv")
+
+
+#Functioning for whole process
+
+def create_rfm(dataframe, csv=False):
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    today_date = dt.datetime(2011, 12, 11)  # Analiz tarihi, son tarihe iki gül eklenerek oluşturuldu.
+
+    rfm = df.groupby("Customer ID").agg({'InvoiceDate': lambda InvoiceDate: (today_date - InvoiceDate.max()).days,
+                                         'Invoice': lambda Invoice: Invoice.nunique(),
+                                         'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+    rfm.columns = ['recency', 'frequency', 'monetary']
+    rfm = rfm[rfm["monetary"] > 0]
+    rfm.describe().T
+
+    rfm["recency_score"] = pd.qcut(rfm["recency"], 5, labels=[5, 4, 3, 2, 1])
+    rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+    rfm["monetary_score"] = pd.qcut(rfm["monetary"], 5, labels=[1, 2, 3, 4, 5])
+
+    rfm["RFM_SCORE"] = (rfm["recency_score"].astype(str) +
+                        rfm["frequency_score"].astype(str))
+
+    seg_map = {
+        r'[1-2][1-2]': 'hibernating',
+        r'[1-2][3-4]': 'at_Risk',
+        r'[1-2]5': 'cant_lose',
+        r'3[1-2]': 'about_to_sleep',
+        r'33': 'need_attention',
+        r'[3-4][4-5]': 'loyal_customers',
+        r'41': 'promising',
+        r'51': 'new_customers',
+        r'[4-5][2-3]': 'potential_loyalists',
+        r'5[4-5]': 'champions',
+    }
+
+    rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
+    rfm = rfm[["recency", "frequency", "monetary", "segment"]]
+    rfm.index = rfm.index.astype(int)
+
+    if csv:
+        rfm.to_csv("rfm.csv")
+
+    return rfm
+
+
+rfm_new = create_rfm(df, csv=True)
